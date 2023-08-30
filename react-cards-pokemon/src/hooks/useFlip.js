f38f3ca916ee9ae329aecbce079bd3fee3f90556
Juan Rojas <juanrojas1912@gmail.com>
2023-08-30 14:12:57 -0400

    react cards pokemon exercise step 2

---
+++ react-cards-pokemon/src/hooks/useFlip.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const useFlip = (initState) => {
+  const [state, setState] = useState(initState);
+  const flipState = () => {
+    setState((state) => !state);
+  };
+
+  return [state, flipState];
+};
+
+export default useFlip;
